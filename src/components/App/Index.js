e40e12ce1a2a321a9e023ea124b81e48d3ca2275
James <james.greygoose@gmail.com>
2020-03-25 17:38:26 +0000

    Pictures added

--- src/components/App/Index.js
+++ src/components/App/Index.js
@@ -1,33 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import benHead from "../../../src/benFixed.png";
 import mrBenBod from "../../../src/benBody.png";
 import Buttons from "../Buttons/buttons";
-
-// BENAROO
-
-// similar to balloon task, players pin requests for help on Ben
-// requests are in the form of buttons with different programming symbols on
-// buttons will generate a random number each time between -3 and 3
-// The number of questions ben can take will be between 10 and 15
-// addition of minus numbers means total can go up or down
-// when number is met then Ben will explode!
-
-// BOARD
-//picture of ben, who's head grows on each click
-//"Speech mark" buttons
-
-//
+import benaroo from "../Buttons/benarooTitle.png";
+import docs from "../Speech/bubbles/chris docs.JPG";
+import errors from "../Speech/bubbles/chris errors.JPG";
+import google from "../Speech/bubbles/chris google.JPG";
+import hive from "../Speech/bubbles/chris hive.JPG";
+import idiot from "../Speech/bubbles/chris idiot.JPG";
+import know from "../Speech/bubbles/chris know.JPG";
+import minutes from "../Speech/bubbles/chris minutes.JPG";
+import nearly from "../Speech/bubbles/chris nearly.JPG";
+import nice from "../Speech/bubbles/chris nice.JPG";
+import plan from "../Speech/bubbles/chris plan.JPG";
 
 function App() {
+  //
   const [size, setSize] = useState(50);
+  const [benLimit, setBenLimit] = useState(51);
+  const [gameOver, setGameOver] = useState("");
+  const [head, setHead] = useState(false);
+  const [picture, setPicture] = useState("");
+
+  const bubbles = [
+    docs,
+    errors,
+    google,
+    hive,
+    idiot,
+    know,
+    minutes,
+    nearly,
+    nice,
+    plan
+  ];
+
+  useEffect(() => {
+    if (size > 70 && size < 100) {
+      setPicture(bubbles[Math.floor(Math.random() * 10)]);
+      console.log("over 70");
+    }
+  }, [size]);
+
+  useEffect(() => {
+    setBenLimit(Math.floor(Math.random() * (250 - 180 + 1) + 180));
+  }, []);
 
   function blowUpBen() {
-    setSize(size + 15);
+    setSize(size + Math.floor(Math.random() * (30 - -2) + -2));
   }
+
+  console.log("size=  " + size);
+  console.log("I can see  " + benLimit);
+
+  useEffect(() => {
+    if (size > benLimit) {
+      console.log("Game Over");
+      setGameOver("GAME OVER!");
+      setHead(true);
+    }
+  }, [size]);
 
   return (
     <div className="App">
+      <img id="benarooTitle" src={benaroo} alt="Benaroo" />
+      <h1>Click on a button below and see how Ben reacts!</h1>
+      <h2>
+        If you're the one who makes his head explode, hang your head in shame!
+      </h2>
       <img
         className="benHead"
         src={benHead}
@@ -35,10 +76,11 @@ function App() {
         style={{ width: size }}
       />
       <img className="benBod" src={mrBenBod} alt="benbod" />
-      <button onClick={blowUpBen}>Ask Ben a Question!</button>
-      <Buttons />
+      <Buttons blowUpBen={blowUpBen} />
+      <p>{gameOver}</p>
+
+      <img src={picture} alt="" />
     </div>
   );
 }
-
 export default App;
--- src/components/Buttons/buttons.js
+++ src/components/Buttons/buttons.js
@@ -1,29 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import "./buttons.css";
+// import blowUpBen from "../App/Index";
+const topics = [
+  "babel",
+  "css",
+  "codeWars",
+  "express",
+  "git",
+  "heroku",
+  "html",
+  "jest",
+  "js",
+  "netlify",
+  "node",
+  "pg",
+  "postman",
+  "react",
+  "vsCode"
+];
 
-function Buttons() {
-  const [size, setSize] = useState(50);
+function Buttons({ blowUpBen }) {
+  // const [size, setSize] = useState(50);
 
-  function blowUpBen() {
-    setSize(size + 15);
-  }
+  // function blowUpBen() {
+  //   setSize(size + 15);
+  // }
   return (
     <div id="buttonDiv">
-      <button onClick={blowUpBen} id="babel" alt="babel"></button>
-      <button onClick={blowUpBen} id="codeWars" alt="codeWars"></button>
-      <button onClick={blowUpBen} id="css" alt="css"></button>
-      <button onClick={blowUpBen} id="express" alt="express"></button>
-      <button onClick={blowUpBen} id="git" alt="gridTemplate: "></button>
-      <button onClick={blowUpBen} id="heroku" alt="heroku"></button>
-      <button onClick={blowUpBen} id="html" alt="html"></button>
-      <button onClick={blowUpBen} id="jest" alt="justifyContent: "></button>
-      <button onClick={blowUpBen} id="js" alt="js"></button>
-      <button onClick={blowUpBen} id="netlify" alt="netlify"></button>
-      <button onClick={blowUpBen} id="node" alt="node"></button>
-      <button onClick={blowUpBen} id="pg" alt="pg"></button>
-      <button onClick={blowUpBen} id="postman" alt="postman"></button>
-      <button onClick={blowUpBen} id="react" alt="react"></button>
-      <button onClick={blowUpBen} id="vsCode" alt="vsCode"></button>
+      {topics.map(topic => (
+        <button onClick={blowUpBen} id={topic} alt={topic}></button>
+      ))}
     </div>
   );
 }
